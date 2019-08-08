eefe3b17304935d71bbc39a476939d8c06e9610f
Henrik Bjørhall <47069120+Orphanum@users.noreply.github.com>
2019-08-08 14:20:30 +0200

    Add files via upload

---
+++ SimpleChess-master/SimpleChess/Piece.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SimpleChess
+{
+    abstract class Piece
+    {
+        public string Symbol { get; }
+
+        public Piece(string symbol)
+        {
+            Symbol = symbol;
+
+
+        }
+        public abstract bool Move(string fromPosition, string toPosition);
+    }
+}
